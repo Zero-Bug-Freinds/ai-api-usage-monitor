c2deacaa86619f6001070654619441350fa6750e
Yena <dpsk1515@gmail.com>
2026-05-14 22:35:44 +0900

    Chore: llmproxy-service 삭제

--- services/llm-proxy-service/src/main/java/com/eevee/llmproxyservice/repository/UsageLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eevee.llmproxyservice.repository;
-
-import com.eevee.llmproxyservice.domain.UsageLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsageLogRepository extends JpaRepository<UsageLog, Long> {
-}
